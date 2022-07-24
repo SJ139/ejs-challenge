1913d553536c1ac843ba2780db326c115d0d955b
SJ139 <90908149+SJ139@users.noreply.github.com>
2022-07-24 09:08:10 -0400

    Sunday Second  updates

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+var posts = ss
+
+
 //Get home page, /about page/contact page.catch(err => {})
 app.get("/", function(req,res){
   res.render("home", {startingContent: homeStartingContent});
