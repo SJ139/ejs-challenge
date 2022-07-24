983b7f86806e5fb9167abf6a833bb04c4fb0ccb7
SJ139 <90908149+SJ139@users.noreply.github.com>
2022-07-24 08:26:24 -0400

    Friday updates

--- app.js
+++ app.js
@@ -30,15 +30,17 @@ app.get("/contact", function(req,res){
 
 app.get("/compose", function(req,res){
   res.render("compose");
+
+});
+
+app.post("/compose", function(req,res){
+    console.log(req.body.posTitle);
+    console.log(req.body.postBody);
 });
 
 // app.post("/compose", function(req,res){
-//     console.log(req.body.posTitle);
+//
 // });
-
-app.post("/compose", function(req,res){
-    console.log(req.body.postBody);
-});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
